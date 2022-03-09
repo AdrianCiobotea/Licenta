5d0b7678f3fb232ead25c46c823f625db645783a
Adrian <ciobotea.adrian5@yahoo.com>
2022-03-10 00:18:06 +0200

    added JWT for login

--- FoodWare/auth/src/main/java/com/foodware/auth/login/LoginService.java
+++ FoodWare/auth/src/main/java/com/foodware/auth/login/LoginService.java
@@ -1,11 +1,42 @@
 //package com.foodware.auth.login;
 //
+//import com.foodware.auth.user.UserDetails;
+//import com.foodware.auth.user.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
 //
-//import lombok.AllArgsConstructor;
-//import org.springframework.stereotype.Service;
+//@Component
+//public class LoginService implements AuthenticationProvider {
+//    @Autowired
+//    private UserService userService;
 //
-//@Service
-//@AllArgsConstructor
-//public class LoginService {
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    public Authentication authenticate(Authentication authentication) {
+//        String email = authentication.getName();
+//        String pwd = authentication.getCredentials().toString();
+//        UserDetails user= userService.loadUserByEmail(email);
+//        if(user == null) {
+//        throw new BadCredentialsException("No user with this email was found");
+//        }
+//            if (passwordEncoder.matches(pwd, customer.get(0).getPwd())) {
+//                return new UsernamePasswordAuthenticationToken(username, pwd, getGrantedAuthorities(customer.get(0).getAuthorities()));
+//            } else {
+//                throw new BadCredentialsException("Invalid password!");
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public boolean supports(Class<?> authenticationType) {
+//        return authenticationType.equals(UsernamePasswordAuthenticationToken.class);
+//    }
 //
 //}
--- FoodWare/auth/src/main/java/com/foodware/auth/security/config/WebSecurityConfig.java
+++ FoodWare/auth/src/main/java/com/foodware/auth/security/config/WebSecurityConfig.java
@@ -1,15 +1,24 @@
 package com.foodware.auth.security.config;
 
-import com.foodware.auth.user.UserService;
+import com.foodware.auth.login.filter.JWTTokenGeneratorFilter;
+import com.foodware.auth.login.filter.JWTTokenValidatorFilter;
+import com.foodware.auth.login.filter.RequestValidationBeforeFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @AllArgsConstructor
@@ -17,14 +26,32 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http
-                .csrf().disable()
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
+                cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }).and().csrf().disable()
+                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/login/**").permitAll()
-                    .antMatchers("/register/**")
-                    .permitAll();
-
+                .antMatchers("/myAccount").hasRole("USER")
+                .antMatchers("/myBalance").hasAnyRole("USER","ADMIN")
+                .antMatchers("/myLoans").hasRole("ROOT")
+                .antMatchers("/myCards").hasAnyRole("USER","ADMIN")
+                .antMatchers("/user").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll().and().httpBasic();
     }
 
 }
